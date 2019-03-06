417df5707fce82955185042e6f034ac9e4c0aafa
Pedro Camargo <dropecamargo@gmail.com>
2019-03-06 17:02:12 -0500

    first commit

---
+++ src/main/java/com/pcamargo/mvc/controllers/HomeController.java
@@ -0,0 +1,23 @@
+package com.pcamargo.mvc.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public ModelAndView index() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("show-form");
+		return mav;
+	}
+	
+	@RequestMapping("/processForm")
+	public ModelAndView processForm() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("success-form");
+		return mav;
+	}
+}
